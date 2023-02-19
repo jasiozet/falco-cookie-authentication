cf9f283b015197c119de70febede1e9233c1339b
Jan Zalewski <18668118+jasiozet@users.noreply.github.com>
2023-02-19 22:48:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# F# Falco Cookie Based ASP.NET Authentication
+
+## How to use it
+
+Clone the repo!
+
+## Requirements
+
+[dotNET 7](https://dotnet.microsoft.com/en-us/download/dotnet/7.0)
+(you can downgrade dotnet version to 6.0 easily in the fsproj files)
+
+Editor capable of working with F#, I personally recommend:
+* [Visual Studio Code](https://code.visualstudio.com/) and [Ionide plugin](https://ionide.io/)
+
+Other options:
+* [Visual Studio](https://visualstudio.microsoft.com/) or [Visual Studio for Mac](https://visualstudio.microsoft.com/vs/mac/)
+* [Rider](https://www.jetbrains.com/rider/)
+
+## Stuff inside API
+
+* [Falco](https://www.falcoframework.com/)
+
+## How to run:
+* api - ```cd api && dotnet run```
+
+## Routes to test:
+* http://localhost:5000/
+* http://localhost:5000/signin
+* http://localhost:5000/signout
---
+++ api/Program.fs
@@ -0,0 +1,60 @@
+﻿open Falco
+open Falco.Routing
+open Falco.HostBuilder
+open Microsoft.AspNetCore.Authentication
+open System.Security.Claims
+open Falco.Markup
+
+[<Literal>]
+let AuthnTypeName = "usr"
+
+let secureResourceHandler : HttpHandler =
+  let handleAuth : HttpHandler =
+    fun ctx ->
+      let name =
+        ctx.User.Identities
+        |> Seq.head
+        |> fun x -> x.Claims
+        |> Seq.filter (fun x -> x.Type = AuthnTypeName)
+        |> Seq.head
+        |> fun x -> x.Value
+      Response.ofPlainText $"hello authenticated user: {name}" ctx
+
+  let handleInvalid : HttpHandler =
+    Response.withStatusCode 403
+    >> Response.ofPlainText "Forbidden"
+
+  Request.ifAuthenticated handleAuth handleInvalid
+
+let signInHandler : HttpHandler =
+  let claims = [Claim(AuthnTypeName, "authn_falconista")]
+  let claimsIdentity = ClaimsIdentity(claims, Cookies.CookieAuthenticationDefaults.AuthenticationScheme)
+  let claimsPrincipal = ClaimsPrincipal(claimsIdentity)
+  Response.signInAndRedirect Cookies.CookieAuthenticationDefaults.AuthenticationScheme claimsPrincipal "/secure"
+
+let signOutHandler : HttpHandler =
+  Response.signOutAndRedirect Cookies.CookieAuthenticationDefaults.AuthenticationScheme "/secure"
+
+let mainPageHandler : HttpHandler =
+  let page = Elem.html [] [
+    Elem.body [] [
+      Elem.a [Attr.href "/signin"] [Text.raw "Sign in"]
+      Elem.hr []
+      Elem.a [Attr.href "/signout"] [Text.raw "Sign out"]
+    ]
+  ]
+  Response.ofHtml page
+
+[<EntryPoint>]
+let main args =
+  webHost args {
+    add_cookie Cookies.CookieAuthenticationDefaults.AuthenticationScheme (fun cookieAuthOptions -> ())
+    use_authentication
+    endpoints [
+        get "/"         mainPageHandler
+        get "/secure"   secureResourceHandler
+        get "/signin"   signInHandler
+        get "/signout"  signOutHandler
+    ]
+  }
+  0
